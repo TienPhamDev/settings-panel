8a4e13a765ae038ebb69a9f2ea727fb723d13338
TienPhamDev <manhtienbn18891@gmail.com>
2024-03-03 18:48:58 -0500

    css,js,darkmode

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const syncBTN = settingBox.children["sync"];
 const visibilityBTN = settingBox.children["visibility"];
 const privacyBTN = settingBox.children["privacy"];
 const deleteBTN = settingBox.children["delete"];
-
+const darkModeBTN = settingBox.children["darkmode"];
 // functions
 function switchButton(button) {
   button.addEventListener("click", (e) => {
@@ -21,28 +21,44 @@ function switchButton(button) {
 function activateButton(button) {
   if (localStorage.getItem(button.name) === "active") {
     button.classList.add("active");
+    if (button.name === "darkmode") {
+      document.body.classList.add("dark-mode");
+    }
   }
 }
 function setDefaultButton(button) {
   if (localStorage.getItem(button.name)==="active") {
     localStorage.removeItem(button.name);
     button.classList.remove("active");
+    if (button.name === "darkmode") {
+      document.body.classList.remove("dark-mode");
+    }
   }
 }
+
 // toggle button switch when clicked and added to localStorage
 switchButton(syncBTN);
 switchButton(visibilityBTN);
 switchButton(privacyBTN);
-
+switchButton(darkModeBTN)
+// change body to darkmode when dark mode button is activated
+darkModeBTN.addEventListener("click", () => {
+  if (darkModeBTN.classList.contains("active")) {
+    document.body.classList.add("dark-mode");
+  } else {
+    document.body.classList.remove("dark-mode");
+  }
+});
 // if button is activated 
 activateButton(syncBTN);
 activateButton(visibilityBTN);
 activateButton(privacyBTN);
-
+activateButton(darkModeBTN);
 // set all buttons to non active state when delete button clicked
 deleteBTN.addEventListener("click", () => {
   setDefaultButton(syncBTN);
   setDefaultButton(visibilityBTN);
   setDefaultButton(privacyBTN);
+  setDefaultButton(darkModeBTN);
 });
 
